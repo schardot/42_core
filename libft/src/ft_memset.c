cea717541386181335b47daa99d2d6a49be82182
Natália Schardosim <natalia.schardosim@hotmail.com>
2024-04-09 15:58:10 +0200

    ft_memset.c not tested

---
+++ libft/src/ft_memset.c
@@ -0,0 +1,11 @@
+void    *ft_memset(void *b, int c, size_t len)
+{
+    unsigned char *ptr = (unsigned char *)b;
+    while (len > 0)
+    {
+        *ptr = (unsigned char)c;
+        len --;
+        ptr ++;
+    }
+    return (b);
+}
