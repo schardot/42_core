5bebc3235e8d3ad303074471c81fbe4a6a76a2b3
Natália Schardosim <natalia.schardosim@hotmail.com>
2024-04-09 15:40:43 +0200

    ft_isascii.c tested

---
+++ libft/src/ft_isascii.c
@@ -0,0 +1,20 @@
+int ft_isascii(int c)
+{
+    return (0 <= c && c <= 127);
+}
+// #include <stdio.h>
+// int main()
+// {
+//     char ch = 's';
+
+//     if (ft_isascii(ch))
+//     {
+//         printf("%c is ascii.\n", ch);
+//     }
+//     else
+//     {
+//         printf("%c is not ascii.\n", ch);
+//     }
+
+//     return 0;
+// }
