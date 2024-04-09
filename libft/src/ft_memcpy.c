4fcec48af1b9f98b6fb344782dfb8e53eb202aa3
Natália Schardosim <natalia.schardosim@hotmail.com>
2024-04-09 18:21:58 +0200

    ft_memcpy.c not tested

---
+++ libft/src/ft_memcpy.c
@@ -0,0 +1,11 @@
+void    *memcpy(void *dest, const void *src, size_t n)
+{
+    while (n > 0)
+    {
+        dest = src;
+        n --;
+        dest ++;
+        src ++;
+    }
+    return (dest);
+}
