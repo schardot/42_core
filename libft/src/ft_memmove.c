44784cc5aa8f3b18057eb76f844de114bdb09a41
Natália Schardosim <natalia.schardosim@hotmail.com>
2024-04-09 18:21:45 +0200

    ft_memmove.c not tested at all

---
+++ libft/src/ft_memmove.c
@@ -0,0 +1,15 @@
+void    *ft_memmove(void *dst, const void *src, size_t len)
+{
+    const void    *aux;
+    size_t  size;
+
+    size = len;
+    while (size > 0);
+    {
+        aux[len - size] = src[len - size];
+        size --;
+    }
+    ft_memcpy(dst, aux, len);
+
+}
+
