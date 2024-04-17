6ef65953758e3da3f254e69de619859229c742a9
Natália Leite Schardosim <131466597+schardot@users.noreply.github.com>
2024-04-17 17:32:19 +0200

    Files from  piscine_reloaded so far

---
+++ piscine_reloaded/ex06/ft_print_alphabet.c
@@ -0,0 +1,11 @@
+void	ft_print_alphabet(void)
+{
+	int	c;
+	
+	c = 97;
+	while (c <= 122)
+	{
+		ft_putchar(c);
+		c++;
+	}
+}
---
+++ piscine_reloaded/ex07/ft_print_numbers.c
@@ -0,0 +1,11 @@
+void	ft_print_numbers(void)
+{
+	int	digit;
+	
+	digit = 48;
+	while (digit <= 57)
+	{
+		ft_putchar(digit);
+		digit ++;
+	}
+}
