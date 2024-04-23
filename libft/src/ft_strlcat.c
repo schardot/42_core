1f892ad078450c0cc162020680b917af83ee8f6f
Natalia Schardosim <natalia.schardosim@hotmail.com>
2024-04-23 16:46:47 +0200

    strlcat tested

--- libft/src/ft_strlcat.c
+++ libft/src/ft_strlcat.c
@@ -1,55 +1,30 @@
 #include "libft.h"
 
-#include <stddef.h>
-
-size_t	ft_strlen(const char *s);
-
-void	ft_bzero(void *s, size_t n);
-
-void	*ft_memset(void *b, int c, size_t len);
-
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 		size_t dstlen;
+		size_t srclen;
 		size_t i;
 
-		ft_bzero(dst, size);
+		srclen = ft_strlen(src);
 		dstlen = ft_strlen(dst);
 		i = dstlen;
-		src += dstlen;
-		while (*src != '\0' && dst[i] != '\0' && i < size)
+		dst += dstlen;
+		while (*src != '\0' && i < size)
 		{
-				*dst = *src;
-				src ++;
-				i ++;
+			*dst ++ = *src ++;
+			i ++;
 		}
 		*dst = '\0';
-		return (dstlen + ft_strlen(src));
-}
-
-size_t	ft_strlen(const char *s)
-{
-	size_t length;
-
-	length = 0;
-	while (*s != '\0')
-	{
-		length ++;
-		s ++;
-	}
-	return (length);
+		dst -= (dstlen + srclen);
+		return (dstlen + srclen);
 }
 
 #include <stdio.h>
 
 int	main(void)
 {
-		char dst[3];
-		printf("the return value is %ld", ft_strlcat(dst, "hello", 3));
+		char	dst[8] = "oi";
+		printf("the return value is %ld\n", ft_strlcat(dst, "hello", 8));
 		printf("this is dst: %s", dst);
-}
-
-void	ft_bzero(void *s, size_t n)
-{
-	ft_memset(s, 0, n);
 }
