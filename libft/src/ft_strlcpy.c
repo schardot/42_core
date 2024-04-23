7b6d539cc1e312e89a164ea6dada770a48b267bf
Natalia Schardosim <natalia.schardosim@hotmail.com>
2024-04-23 13:49:10 +0200

    ft_strlcpy tested

--- libft/src/ft_strlcpy.c
+++ libft/src/ft_strlcpy.c
@@ -1,15 +1,24 @@
 #include "libft.h"
+#include <stdio.h>
+#include <string.h>
 
-size_t  strlcpy(char *restrict dst, const char *restrict src, size_t dstsize)
+size_t  ft_strlcpy(char *restrict dst, const char *restrict src, size_t dstsize)
 {
-    int i;
-
-    i = 0
-    while (src[i] != '\0' && i > dstsize)
-    {
-        dst[i] = src[i];
-        i ++;
-    }
-    dst[i] = '\0';
-    return (strlen(src));
+    if (dstsize == 0)
+        return (ft_strlen(src));
+    ft_memcpy(dst, src, dstsize - 1);
+    dst[dstsize - 1] = '\0';
+    return (ft_strlen(src));
 }
+
+// int main(void)
+// {
+//     char    dst[] = "hi";
+//     char    src[] = "oaaaaaa";
+
+//     printf("dst before strlcpy: %s\n", dst);
+//     ft_strlcpy(dst, src, sizeof(char) * 3);
+//     printf("dst after strlcpy: %s\n", dst);
+//     printf("the return value for ft_strlcpy is: %ld\n", ft_strlcpy(dst, src, sizeof(char) * 3));
+//     printf("and the return value for THE REAL strlcpy is: %d\n", strlcpy(dst, src, sizeof(char) * 3));
+// }
