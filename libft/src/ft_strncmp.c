d855f57f104b43df3d10934541faa254d6e6f164
Natália Schardosim <natalia.schardosim@hotmail.com>
2024-05-04 12:15:27 +0200

    corrected minor errors in strncmp

--- libft/src/ft_strncmp.c
+++ libft/src/ft_strncmp.c
@@ -1,14 +1,14 @@
-#include <libft.h>
+#include "libft.h"
 
 int strncmp(const char *str1, const char *str2, size_t n)
 {
     unsigned int i;
 
     i = 0;
-    while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
+    while ((str1[i] != '\0' || str2[i] != '\0') && i < n)
     {
-        if (s1[i] != s2[i])
-            return (s1[i] - s2[i]);
+        if (str1[i] != str2[i])
+            return (str1[i] - str2[i]);
         i++;
     }
     return (0);
