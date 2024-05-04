29bce52e691ff3e9d492f867a3002472dd87625a
Natália Schardosim <natalia.schardosim@hotmail.com>
2024-05-04 12:22:53 +0200

    added a ";" that id forgotten

--- libft/src/ft_strmapi.c
+++ libft/src/ft_strmapi.c
@@ -8,7 +8,7 @@ char    *ft_strmapi(char const *s, char (*f)(unsigned int, char))
     i = 0;
     result = (char *)malloc(ft_strlen(s + 1));
     if (result == NULL)
-        return (NULL)
+        return (NULL);
     while (s[i] != '\0')
     {
         result[i] = f(i, s[i]);
