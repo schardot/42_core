a7120dcf164917a677d537329f761416aa03782b
Natália Schardosim <natalia.schardosim@hotmail.com>
2024-05-04 13:07:21 +0200

    took out the restrict in strlcpy

--- libft/src/libft.h
+++ libft/src/libft.h
@@ -15,7 +15,7 @@ void    *ft_memcpy(void *dest, const void *src, size_t n);
 void    *ft_memmove(void *dst, const void *src, size_t len);
 void    ft_bzero(void *s, size_t n);
 size_t  ft_strlcat(char *dst, const char *src, size_t size);
-size_t  ft_strlcpy(char *restrict dst, const char *restrict src, size_t dstsize);
+size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t  ft_strlen(const char *s);
 char    *ft_strnstr(const char *big, const char *little, size_t len);
 char    *ft_strrchr(const char *s, int c);
