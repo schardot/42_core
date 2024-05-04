5867138eec2fc847e6f828c6bfc83360089fe06c
Natália Schardosim <natalia.schardosim@hotmail.com>
2024-05-04 18:48:48 +0200

    commented out the main

--- libft/src/ft_toupper.c
+++ libft/src/ft_toupper.c
@@ -3,15 +3,15 @@
 int ft_toupper(int c)
 {
 	if (ft_isalpha(c) && (c >= 'a' && c <= 'z'))
-		return (c -= 32);   
+		return (c -= 32);
 	return (c);
 }
-#include <stdio.h>
-int	main(void)
-{
-	//char a = 'a'; - should upper
-	//char a = 'A'; - should leave it alone
-	char a = '0'; // - should leave it alone
-	a = ft_toupper(a);
-	printf("%c", a);
-}
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	//char a = 'a'; - should upper
+// 	//char a = 'A'; - should leave it alone
+// 	char a = '0'; // - should leave it alone
+// 	a = ft_toupper(a);
+// 	printf("%c", a);
+// }
