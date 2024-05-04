35c2715fa2784b4416de27412d4e59b0596d0556
Natália Schardosim <natalia.schardosim@hotmail.com>
2024-05-04 11:30:21 +0200

    wrote strmapi

--- libft/src/ft_strmapi.c
+++ libft/src/ft_strmapi.c
@@ -5,11 +5,15 @@ char    *ft_strmapi(char const *s, char (*f)(unsigned int, char))
     char *result;
     unsigned int i;
 
+    i = 0;
     result = (char *)malloc(ft_strlen(s + 1));
     if (result == NULL)
         return (NULL)
     while (s[i] != '\0')
     {
         result[i] = f(i, s[i]);
+        i ++;
     }
+    result[i] = '\0';
+    return (result);
 }
