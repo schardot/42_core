2d756504ed77cc9ffbc5312cbb8277d5befde111
Natalia Schardosim <natalia.schardosim@hotmail.com>
2024-05-13 13:25:09 +0200

    added bonus functions to libft, also the struct def

--- libft/src/libft.h
+++ libft/src/libft.h
@@ -1,14 +1,14 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nleite-s <nleite-s@student.42berlin.d      +#+  +:+       +#+        */
+/*   By: nleite-s <nleite-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 17:04:01 by nleite-s          #+#    #+#             */
-/*   Updated: 2024/05/06 17:04:03 by nleite-s         ###   ########.fr       */
+/*   Updated: 2024/05/13 11:47:13 by nleite-s         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #ifndef LIBFT_H
 # define LIBFT_H
@@ -17,6 +17,12 @@
 # include <unistd.h>
 # include <stddef.h>
 # include <limits.h>
+
+typedef struct      s_list
+{
+	void            *content;
+	struct s_list   *next;
+}                   t_list;
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -52,5 +58,17 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void
+*));
+void	ft_lstclear(t_list **lst, void (*del)(void
+*));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *),
+void (*del)(void *));
 
 #endif
