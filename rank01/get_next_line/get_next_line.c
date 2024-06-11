1df3dff7951b662a6466682e6d63b2174dafa3fb
Natália Schardosim <natalia.schardosim@hotmail.com>
2024-06-11 18:17:36 +0200

    working but no norminette

--- rank01/get_next_line/get_next_line.c
+++ rank01/get_next_line/get_next_line.c
@@ -1,44 +1,13 @@
 #include "get_next_line.h"
 #include <stdio.h>
 #include <fcntl.h>
+#include <unistd.h>
 
-char    *get_next_line(int fd)
+void *ft_realloc(void *ptr, size_t size)
 {
-    char    byte;
-    char    *line;
-    int len;
-    int b;
-
-    len = 1;
-    b = read(fd, &byte, 1);
-	line = (char *)malloc(1);
-	if (!line)
-		return (NULL);
-	*line = byte;
-    while (byte != '\n')
-    {
-        line = (char *)ft_realloc(line, len + 1 * sizeof(char));
-        read(fd, &byte, 1);
-		line[len] = byte;
-        len ++;
-    }
-    return (line);
-}
-
-int main(void)
-{
-    char *r;
-    int fd;
-
-    fd = open("test.txt", O_RDONLY);
-    r = get_next_line(fd);
-    printf("%s", r);
-    close(fd);
-}
-
-void	*ft_realloc(void *ptr, size_t size)
-{
-	void	*newptr;
+	if (!ptr)
+		return (malloc(size));
+	void *newptr;
 	newptr = malloc(size);
 	if (!newptr)
 		return (NULL);
@@ -46,10 +15,88 @@ void	*ft_realloc(void *ptr, size_t size)
 	return (newptr);
 }
 
+char *get_next_line(int fd)
+{
+	static char	*extra;
+	char *buffer;
+	char	*line;
+	int	b;
+	int i;
+	int linelen;
+	int x;
+
+	x = 0;
+	linelen = 0;
+	if (extra)
+	{
+		line = NULL;
+		line = ft_realloc(line, 1 + linelen);
+		while (*extra != '\0')
+		{
+			line[linelen] = *extra;
+			if (line[linelen] == '\n' || line[linelen] == EOF)
+			{
+				extra ++;
+				return (line);
+			}
+			linelen++;
+			extra++;
+		}
+		//free(extra);
+	}
+	buffer = (char *)malloc(BUFFER_SIZE);
+	if (!buffer)
+		return (NULL);
+	while (1)
+	{
+		b = read(fd, buffer, BUFFER_SIZE);
+		if (b == 0)
+			return (NULL);
+		i = 0;
+		while (i < BUFFER_SIZE)
+		{
+			line = ft_realloc(line, 1 + linelen);
+			line[linelen] = buffer[i];
+			if (line[linelen] == '\n' || line[linelen] == EOF)
+			{
+				while (b > i + 1)
+				{
+					extra = ft_realloc(extra, x);
+					extra[x] = buffer[i + 1];
+					x ++;
+					i ++;
+				}
+				extra = ft_realloc(extra, x);
+				extra[x] = '\0';
+				free(buffer);
+				return (line);
+			}
+			i++;
+			linelen ++;
+		}
+	}
+}
+
+int main(void)
+{
+    char *r;
+    int fd;
+
+	r = "";
+    fd = open("test.txt", O_RDONLY);
+	while (r)
+	{
+		r = get_next_line(fd);
+		printf("%s", r);
+	}
+    close(fd);
+}
+
 void *ft_memcpy(void *dest, const void *src, size_t n)
 {
 	char *d;
 	const char *s;
+	int i;
 
 	d = dest;
 	s = src;
@@ -57,11 +104,12 @@ void *ft_memcpy(void *dest, const void *src, size_t n)
 	{
 		return (NULL);
 	}
+	i = 0;
 	while (n > 0)
 	{
-		*d++ = *s++;
-		n--;
+		d[i] = s[i];
+		n --;
+		i ++;
 	}
 	return (dest);
 }
-
--- rank01/get_next_line/get_next_line.h
+++ rank01/get_next_line/get_next_line.h
@@ -4,8 +4,19 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 10000000
+#endif
+
 char *get_next_line(int fd);
-void *ft_realloc(void *ptr, size_t size);
+size_t ft_strlcat(char *dst, const char *src, size_t size);
+char *check_buffer(char *buffer);
+size_t ft_strlen(const char *s);
+char *ft_substr(char const *s, unsigned int start, size_t len);
+void *ft_memset(void *b, int c, size_t len);
+char *ft_strchr(const char *s, int c);
+size_t ft_strlcpy(char *dst, const char *src, size_t dstsize);
 void *ft_memcpy(void *dest, const void *src, size_t n);
+char *ft_strdup(const char *str);
 
 #endif
