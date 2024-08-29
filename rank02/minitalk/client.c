2814beafe004bff1dd4afd9603d512d621649a5c
Natália Schardosim <natalia.schardosim@hotmail.com>
2024-08-29 16:24:17 +0200

    trying to restart to make it work

--- rank02/minitalk/client.c
+++ rank02/minitalk/client.c
@@ -1,36 +1,79 @@
-#include "include/minitalk.h"
-#include "libft/libft.h"
-#include "libft/ft_printf/ft_printf.h"
+#include <stdlib.h>
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[])
+void signalhandler(int sig, siginfo_t *info, void *ucontext);
+
+int main(int argc, char **argv)
 {
     int pid;
-    int i;
+    struct sigaction action;
 
-    if (argc != 3)
-    {
-        ft_printf("Error, wrong arguments.");
-        return (1);
-    }
-    int b;
-    int letter;
-
+    action.sa_sigaction = signalhandler;
+    action.sa_flags = SA_SIGINFO;
+    sigemptyset(&action.sa_mask);
     pid = atoi(argv[1]);
-    i = 0;
-    while (argv[2][i] != '\0')
-    {
-        b = 7;
-        while (b >= 0)
-        {
-            letter = argv[2][i];
-            letter = letter >> b;
-            if ((letter & 1) == 1)
-                kill(pid, SIGUSR2);
-            else
-                kill(pid, SIGUSR1);
-            b--;
-            usleep(100);
-        }
-        i ++;
-    }
+    sigaction(SIGUSR1, &action, NULL);
+    kill(pid, SIGUSR1);
+    while (1)
+        pause();
 }
+
+void signalhandler(int sig, siginfo_t *info, void *ucontext)
+{
+    printf("CLIENT: recebi o sinal %d do processo %d.\n", sig, info->si_pid);
+}
+
+// #include "include/minitalk.h"
+// #include "libft/libft.h"
+// #include "libft/ft_printf/ft_printf.h"
+// #include <signal.h>
+// #include <unistd.h>
+
+// int main(int argc, char *argv[])
+// {
+//     if (argc != 3)
+//     {
+//         ft_printf("Usage: %s <PID> <message>\n", argv[0]);
+//         return (1);
+//     }
+
+//     int pid = ft_atoi(argv[1]);
+//     if (pid <= 0)
+//     {
+//         ft_printf("Invalid PID.\n");
+//         return (1);
+//     }
+
+//     int i = 0;
+//     while (argv[2][i] != '\0')
+//     {
+//         int b = 7;
+//         int letter = argv[2][i];
+//         while (b >= 0)
+//         {
+//             if ((letter & (1 << b)) != 0)
+//             {
+//                 if (kill(pid, SIGUSR2) == -1)
+//                 {
+//                     ft_printf("Failed to send SIGUSR2.\n");
+//                     return (1);
+//                 }
+//             }
+//             else
+//             {
+//                 if (kill(pid, SIGUSR1) == -1)
+//                 {
+//                     ft_printf("Failed to send SIGUSR1.\n");
+//                     return (1);
+//                 }
+//             }
+//             b--;
+//             usleep(100); // Short delay to ensure the signal is processed correctly
+//         }
+//         i++;
+//     }
+
+//     return 0;
+// }
--- rank02/minitalk/server.c
+++ rank02/minitalk/server.c
@@ -1,66 +1,97 @@
-#include <signal.h>
+#include <stdio.h>
 #include <unistd.h>
-#include "include/minitalk.h"
-#include "libft/libft.h"
-#include "libft/ft_printf/ft_printf.h"
+#include <signal.h>
 
-void handle_sigusr(int sig);
-char *ft_stradd_back(char c, char *str);
+void signalhandler(int sig, siginfo_t *info, void *ucontext);
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    ft_printf("%d\n", (int)getpid());
-    signal(SIGUSR1, handle_sigusr);
-    signal(SIGUSR2, handle_sigusr);
+    struct sigaction action;
+
+    action.sa_sigaction = signalhandler;
+    action.sa_flags = SA_SIGINFO;
+    sigemptyset(&action.sa_mask);
+    printf("SERVER PID: %d\n", (int)getpid());
+    sigaction(SIGUSR1, &action, NULL);
     while (1)
-    {
-        sleep(1);
-    }
-
+        pause();
 }
 
-void    handle_sigusr(int sig)
+void signalhandler(int sig, siginfo_t *info, void *ucontext)
 {
-    static int num = 0;
-    static int bit = 0;
-    static char *str = NULL;
-
-    num = num << 1;
-    if (sig == SIGUSR2)
-        num |= 1;
-    bit ++;
-    if (bit == 8)
-    {
-        str = ft_stradd_back((char)num, str);
-        if (!str)
-        {
-            ft_printf("Error allocating memory.\n");
-            exit(1);
-        }
-        if (num == 0)
-        {
-            ft_printf("%s\n", str);
-            free(str);
-            str = NULL;
-        }
-        bit = 0;
-        num = 0;
-    }
+    printf("SERVER: recebi o sinal %d do processo %d.\n", sig, info->si_pid);
+    kill(info->si_pid, sig);
 }
 
-char    *ft_stradd_back(char c, char *str)
-{
-    char    *newstr;
-    size_t  strlen;
+// #include <signal.h>
+// #include <unistd.h>
+// #include "include/minitalk.h"
+// #include "libft/libft.h"
+// #include "libft/ft_printf/ft_printf.h"
 
-    strlen = ft_strlen(str);
-    newstr = (char *)malloc(strlen + 2);
-    if (!newstr)
-        return (NULL);
-    if (str)
-        ft_strlcpy(newstr, str, strlen + 2);
-    newstr[strlen] = c;
-    newstr[strlen + 1] = '\0';
-    free (str);
-    return (newstr);
-}
+// void handle_sigusr(int sig);
+// char *ft_stradd_back(char c, char *str);
+
+// int main(void)
+// {
+// 	struct sigaction action;
+
+// 	action.sa_handler = handle_sigusr;
+// 	sigemptyset(&action.sa_mask);
+// 	action.sa_flags = SA_RESTART;
+// 	sigaction(SIGUSR1, &action, NULL);
+// 	sigaction(SIGUSR2, &action, NULL);
+
+// 	ft_printf("%d\n", (int)getpid());
+//         while (1)
+//     {
+//         sleep(1);
+//     }
+
+// }
+
+// void    handle_sigusr(int sig)
+// {
+//     static int num = 0;
+//     static int bit = 0;
+//     static char *str = NULL;
+
+//     num = num << 1;
+//     if (sig == SIGUSR2)
+//         num |= 1;
+//     bit ++;
+//     if (bit == 8)
+//     {
+//         str = ft_stradd_back((char)num, str);
+//         if (!str)
+//         {
+//             ft_printf("Error allocating memory.\n");
+//             exit(1);
+//         }
+//         if (num == 0)
+//         {
+//             ft_printf("%s\n", str);
+//             free(str);
+//             str = NULL;
+//         }
+//         bit = 0;
+//         num = 0;
+//     }
+// }
+
+// char    *ft_stradd_back(char c, char *str)
+// {
+//     char    *newstr;
+//     size_t  strlen;
+
+//     strlen = ft_strlen(str);
+//     newstr = (char *)malloc(strlen + 2);
+//     if (!newstr)
+//         return (NULL);
+//     if (str)
+//         ft_strlcpy(newstr, str, strlen + 2);
+//     newstr[strlen] = c;
+//     newstr[strlen + 1] = '\0';
+//     free (str);
+//     return (newstr);
+// }
