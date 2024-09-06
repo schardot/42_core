71a9fd39c3b9bc0eee0b13bc78e1c9f18503d17f
Natália Schardosim <natalia.schardosim@hotmail.com>
2024-09-06 11:10:36 +0200

    norminetted things a bit

--- rank02/minitalk/client.c
+++ rank02/minitalk/client.c
@@ -1,61 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nataliaschardosim <nataliaschardosim@st    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/05 10:10:58 by nataliascha       #+#    #+#             */
+/*   Updated: 2024/09/05 10:11:00 by nataliascha      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include <signal.h>
 #include <stdio.h>
 #include <unistd.h>
 
-void parsestring(char *str, int serverpid);
-void sendnull(int serverpid);
+void	parsestring(char *str, int serverpid);
+void	sendnull(int serverpid);
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    int pid;
+	int	pid;
 
-    if (argc != 3)
-        return(1);
-
-    pid = ft_atoi(argv[1]);
-
-    parsestring(argv[2], pid);
-    while (1)
-        usleep(100);
-    }
-
-void    parsestring(char *str, int serverpid)
-{
-    static char ch;
-    static int i;
-    static int b;
-
-    i = 0;
-    while (str[i] != '\0')
-    {
-        b = 7;
-        ch = str[i];
-        while (b >= 0)
-        {
-            if ((ch >> b) & 1)
-                kill(serverpid, SIGUSR2);
-            else
-                kill(serverpid, SIGUSR1);
-            b --;
-            usleep(100);
-        }
-        i ++;
-    }
-    sendnull(serverpid);
-    exit(1);
+	if (argc != 3)
+		return (1);
+	pid = ft_atoi(argv[1]);
+	parsestring(argv[2], pid);
+	while (1)
+		usleep(100);
 }
 
-void    sendnull(int serverpid)
+void	parsestring(char *str, int serverpid)
 {
-    int i;
+	static char	ch;
+	static int	i;
+	static int	b;
 
-    i = 0;
-    while (i < 8)
-    {
-        kill(serverpid, SIGUSR1);
-        i ++;
-        usleep(100);
-    }
+	i = 0;
+	while (str[i] != '\0')
+	{
+		b = 7;
+		ch = str[i];
+		while (b >= 0)
+		{
+			if ((ch >> b) & 1)
+				kill(serverpid, SIGUSR2);
+			else
+				kill(serverpid, SIGUSR1);
+			b --;
+			usleep(100);
+		}
+		i ++;
+	}
+	sendnull(serverpid);
+	exit(1);
 }
 
+void	sendnull(int serverpid)
+{
+	int	i;
+
+	i = 0;
+	while (i < 8)
+	{
+		kill(serverpid, SIGUSR1);
+		i ++;
+		usleep(100);
+	}
+}
--- rank02/minitalk/server.c
+++ rank02/minitalk/server.c
@@ -1,48 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nataliaschardosim <nataliaschardosim@st    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/05 10:11:01 by nataliascha       #+#    #+#             */
+/*   Updated: 2024/09/05 10:11:02 by nataliascha      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
 #include <stdlib.h>
 
-void    signalhandler(int signum, siginfo_t *info, void *context)
+void	signalhandler(int signum, siginfo_t *info, void *context)
 {
-    static int c = 0;
-    static int b = 0;
+	static int	c = 0;
+	static int	b = 0;
 
-    (void)context;
-    (void)info;
-    if (signum == SIGUSR1 || signum == SIGUSR2)
-    {
-        c <<= 1;
-        if (signum == SIGUSR2)
-            c |= 1;
-        b ++;
-        if (b == 8)
-        {
-            write(1, &c, 1);
-            c = 0;
-            b = 0;
-        }
-    }
+	(void)context;
+	(void)info;
+	if (signum == SIGUSR1 || signum == SIGUSR2)
+	{
+		c <<= 1;
+		if (signum == SIGUSR2)
+			c |= 1;
+		b ++;
+		if (b == 8)
+		{
+			write(1, &c, 1);
+			c = 0;
+			b = 0;
+		}
+	}
 }
 
-int main(void)
+int	main(void)
 {
-    struct sigaction action;
+	struct sigaction	action;
 
-    action.sa_sigaction = signalhandler;
-    action.sa_flags = SA_SIGINFO;
-    sigemptyset(&action.sa_mask);
-    if (sigaction(SIGUSR1, &action, NULL) == -1)
-    {
-        perror("sigaction");
-        exit(EXIT_FAILURE);
-    }
-    if (sigaction(SIGUSR2, &action, NULL) == -1)
-    {
-        perror("sigaction");
-        exit(EXIT_FAILURE);
-    }
-    printf("SERVER PID: %d\n", (int)getpid());
-    while (1)
-        pause();
+	action.sa_sigaction = signalhandler;
+	action.sa_flags = SA_SIGINFO;
+	sigemptyset(&action.sa_mask);
+	if (sigaction(SIGUSR1, &action, NULL) == -1)
+	{
+		perror("sigaction");
+		exit(EXIT_FAILURE);
+	}
+	if (sigaction(SIGUSR2, &action, NULL) == -1)
+	{
+		perror("sigaction");
+		exit(EXIT_FAILURE);
+	}
+	printf("SERVER PID: %d\n", (int)getpid());
+	while (1)
+		pause();
 }
