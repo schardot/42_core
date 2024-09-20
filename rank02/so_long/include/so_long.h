af91d4f7bb1d453d0312e19daeac7dc1c3178253
Natália Schardosim <natalia.schardosim@hotmail.com>
2024-09-20 15:48:51 +0200

    i moved the struct declaration to the header

--- rank02/so_long/include/so_long.h
+++ rank02/so_long/include/so_long.h
@@ -6,4 +6,15 @@
 #include <stdio.h>
 #include <errno.h>
 
+typedef struct v_list
+{
+    void *connection;
+    void *window;
+    void *img;
+    void *wallimage;
+    void *playerimage;
+    void *coinimage;
+    void *exitimage;
+} gamedata;
+
 #endif
