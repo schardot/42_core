72d8fdb8c883ea5c0abac2aa751be89a4c8da2ad
Natália Schardosim <natalia.schardosim@hotmail.com>
2024-09-23 17:38:51 +0200

    progress of the day

--- rank02/so_long/srcs/map_utils.c
+++ rank02/so_long/srcs/map_utils.c
@@ -1,7 +1,7 @@
-#include "include/minilibx_opengl_20191021/mlx.h"
-#include "include/so_long.h"
-#include "include/libft/libft.h"
-#include "include/libft/get_next_line/get_next_line.h"
+#include "../include/minilibx_opengl_20191021/mlx.h"
+#include "../include/so_long.h"
+#include "../include/libft/libft.h"
+#include "../include/libft/get_next_line/get_next_line.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
@@ -23,64 +23,93 @@ char **ber_to_grid(int fd) // it will create and populate a grid with informatio
     while (line)
     {
         line = get_next_line(fd);
-        map = ft_realloc(map, height + 2);
+        map = ft_grdrealloc(map, height + 2);
         map[m] = ft_strdup(line);
         m++;
         height++;
     }
-    check_map(map);
+    //check_map(map);
     return (map);
 }
 
-void check_map(char **map)
+// void check_map(char **map)
+// {
+//     1. check if all the borders are 1(walls)
+
+//         int w;
+//     int h;
+
+//     // for now i declared variables so i can count E, P and C but im sure theres a more efficient way to do it.
+//     int E;
+//     int P;
+//     int C;
+//     E = 0;
+//     P = 0;
+//     C = 0;
+
+//     w = 0;
+//     h = 0;
+//     while (map[h])
+//     {
+//         while (map[h][w])
+//         {
+//             if ((h == 0 || h == ultima linha) && map[h][w] != 1)
+//             {
+//                 printf("Map borders are invalid, check the extremities up or down");
+//                 exit(1);
+//             }
+//             else if (first or last letter of the line are not 1)
+//             {
+//                 printf("Map borders are invalid, check the extremities left or right");
+//                 exit(1);
+//             }
+//             if (map[h][w] == 'E')
+//                 E++;
+//             else if (map[h][w] == 'P')
+//                 P++;
+//             else if (map[h][w] == 'C')
+//                 C++;
+//             else if (map[h][w] != '1' && map[h][w] != '0')
+//             {
+//                 ft_printf("Invalid character");
+//                 exit(1);
+//             }
+//             w++;
+//         }
+//         h++;
+//     }
+//     if (E > 1 || P > 1 || C < 1)
+//     {
+//         ft_printf("Too many exits, players or too little coins.");
+//         exit(1);
+//     }
+// }
+
+void grid_to_img(char **map, gamedata *d)
 {
-    1. check if all the borders are 1(walls)
-
-        int w;
+    int w;
     int h;
+    int size;
 
-    // for now i declared variables so i can count E, P and C but im sure theres a more efficient way to do it.
-    int E;
-    int P;
-    int C;
-    E = 0;
-    P = 0;
-    C = 0;
-
-    w = 0;
+    size = 32;
     h = 0;
     while (map[h])
     {
+        w = 0;
         while (map[h][w])
         {
-            if ((h == 0 || h == ultima linha) && map[h][w] != 1)
-            {
-                ft_printf("Map borders are invalid, check the extremities up or down");
-                exit(1);
-            }
-            else if (first or last letter of the line are not 1)
-            {
-                ft_printf("Map borders are invalid, check the extremities left or right");
-                exit(1);
-            }
-            if (map[h][w] == 'E')
-                E++;
+            if (map[h][w] == '1')
+                mlx_put_image_to_window(d->connection, d->window, d->wallimage, size, size);
+            else if (map[h][w] == '0')
+                mlx_put_image_to_window(d->connection, d->window, d->tileimage, size, size);
             else if (map[h][w] == 'P')
-                P++;
+                mlx_put_image_to_window(d->connection, d->window, d->playerimage, size, size);
             else if (map[h][w] == 'C')
-                C++;
-            else if (map[h][w] != '1' && map[h][w] != '0')
-            {
-                ft_printf("Invalid character");
-                exit(1);
-            }
-            w++;
+                mlx_put_image_to_window(d->connection, d->window, d->coinimage, size, size);
+            else if (map[h][w] == 'E')
+                mlx_put_image_to_window(d->connection, d->window, d->exitimage, size, size);
+            w ++;
         }
-        h++;
-    }
-    if (E > 1 || P > 1 || C < 1)
-    {
-        ft_printf("Too many exits, players or to little coins.");
-        exit(1);
+        h ++;
     }
 }
--- rank02/so_long/srcs/so_long.c
+++ rank02/so_long/srcs/so_long.c
@@ -8,48 +8,63 @@
 #include <string.h>
 
 int ft_update(gamedata *g);
+char **ber_to_grid(int fd);
 
 int main(int argc, char **argv)
 {
-    void    *connection;
-    void    *window;
-    void    *img;
-    //arguments_check(argc, argv);
-    connection = mlx_init();
-    if (!connection)
-        exit(1);
-    window = mlx_new_window(connection, 800, 500, "window"); //tem q vincular esse tamanho com o tamanho do mapa
+    gamedata *data;
+    char **grid;
 
-    int size;
-    size = 200;
-    img = mlx_xpm_file_to_image(connection, "/Users/nataliaschardosim/42_core/rank02/so_long/sprites/gold.xpm", &size, &size);
-    if (img == NULL)
-    {
-        printf("Failed to load image\n");
-        return (1);
-    }
-
-    gamedata *g;
-    g = (gamedata *)malloc(1 * sizeof(gamedata));
-    g->connection = connection;
-    g->window = window;
-    g->img = img;
-
-   mlx_loop_hook(connection, ft_update, (void *)g);
-    if (!window)
-        exit(1);
-    mlx_loop(connection);
+    //antes de inicializar struct, arg check
+    grid = start_map(argv[1]);
+    data = init_data(data);
 }
 
 int ft_update(gamedata *g)
 {
-    mlx_put_image_to_window(g->connection, g->window, g->img, 200, 200);
+    mlx_put_image_to_window(g->connection, g->window, g->img, 100, 100);
     return (0);
 }
-
-int put_file_to_image(gamedata *data)
+gamedata *init_data(gamedata *data)
+{
+    data = (gamedata *)malloc(1 * sizeof(gamedata));
+    data->connection = mlx_init();
+    if (!data->connection)
+    {
+        printf("In init_data.c: Can't establish connection");
+        exit(1);
+    }
+    data->window = mlx_new_window(data->connection, 800, 500, "so_long, farewell, auf wiedersehen goodbye!"); // tem q vincular esse tamanho com o tamanho do mapa
+    if (!data->window)
+    {
+        printf("In init_data.c: Can't open window");
+        exit(1);
+    }
+    put_file_to_image(data);
+    mlx_loop_hook(data->connection, ft_update, (void *)data);
+    mlx_loop(data->connection);
+    return (data);
+}
+void put_file_to_image(gamedata *data)
 {
     //tem q initializar data?
     int size = 32;
-    data->coinimage = mlx_xpm_file_to_image(data->connection, "/Users/nataliaschardosim/42_core/rank02/so_long/sprites/gold.xpm", &size, &size);
+    data->coinimage = mlx_xpm_file_to_image(data->connection, "/Users/nataliaschardosim/42_core/rank02/so_long/sprites/collectible.xpm", &size, &size);
+    data->exitimage = mlx_xpm_file_to_image(data->connection, "/Users/nataliaschardosim/42_core/rank02/so_long/sprites/exit.xpm", &size, &size);
+    data->wallimage = mlx_xpm_file_to_image(data->connection, "/Users/nataliaschardosim/42_core/rank02/so_long/sprites/wall.xpm", &size, &size);
+    data->playerimage = mlx_xpm_file_to_image(data->connection, "/Users/nataliaschardosim/42_core/rank02/so_long/sprites/exit.xpm", &size, &size);
+    data->tileimage = mlx_xpm_file_to_image(data->connection, "/Users/nataliaschardosim/42_core/rank02/so_long/sprites/tile.xpm", &size, &size);
+}
+char **start_map(char *filename)
+{
+    int fd;
+    char **grid;
+
+    fd = open(filename, O_RDONLY);
+    if (fd < 0)
+    {
+        printf("In start_map.c: Error opening the file.");
+        exit (1);
+    }
+    return (ber_to_grid(fd));
 }
