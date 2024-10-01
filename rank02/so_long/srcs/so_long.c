c92dc5dfd51217405fb51fd7dacd1907ea4d3a11
Natália Schardosim <natalia.schardosim@hotmail.com>
2024-10-01 15:19:12 +0200

    worked on key_press

--- rank02/so_long/srcs/so_long.c
+++ rank02/so_long/srcs/so_long.c
@@ -25,21 +25,59 @@ int main(int argc, char **argv)
 }
 int	key_press(int key, t_game *gm)
 {
+	char	*next;
+
+	next = check_next_obj(gm, key);
+	check_next_move(next, gm);
+}
+void	check_next_move(char *next, t_game gm)
+{
 	static int	coin = 0;
-	if(check_valid_move())
+	static char	*put_exit = 0;
+
+
+	if (*next == 'C')
 	{
-		gm->map[pl_y][pl_x] = '0';
-		if (key == RIGHT)
-			gm->map[pl_y][pl_x + 1] = 'P';
-		else if (key == LEFT)
-			gm->map[pl_y][pl_x - 1] = 'P';
-		else if (key == UP)
-			gm->map[pl_y + 1][pl_x] = 'P';
-		else if (key == DOWN)
-			gm->map[pl_y - 1][pl_x] = 'P';
-		else if (key == ESC)
-			fechar janela;
+		coin ++;
+		*next = 'P';
+		if (put_exit)
+			gm->map[pl_y][pl_x] = 'E';
+		else
+			gm->map[pl_y][pl_x] = '0';
 	}
+	else if (*next == '0')
+	{
+		*next = 'P';
+		if (put_exit)
+			gm->map[pl_y][pl_x] = 'E';
+		else
+			gm->map[pl_y][pl_x] = '0';
+	}
+	else if (*next == 'E')
+	{
+		if (coin == gm->count_C)
+			mlx_destroy_window(gm->cnct, gm->win);
+		else
+		{
+			*next = 'P';
+			gm->map[pl_y][pl_x] = '0';
+			put_exit = 1;
+		}
+	}
+}
+
+char	*check_next_obj(t_game *gm, int key)
+{
+	if (key == RIGHT)
+		return (&gm->map[pl_y][pl_x + 1]);
+	else if (key == LEFT)
+		return (&gm->map[pl_y][pl_x - 1]);
+	else if (key == UP)
+		return (&gm->map[pl_y + 1][pl_x]);
+	else if (key == DOWN)
+		return (&gm->map[pl_y - 1][pl_x]);
+	else if (key == ESC)
+		mlx_destroy_window(gm->cnct, gm->win);
 }
 
 int main_loop(t_game *gm)
@@ -82,21 +120,21 @@ void	render_game(char **map, t_game *gm)
 
 void init_game_struct(t_game *gm, t_gerr er, t_map m)
 {
-	int	size;
-
-	size = 32;
+	gm->size = 32;
 	gm->cnct = mlx_init();
 	if (!gm->cnct)
 		er->cnct = 1;
 	gm->win = mlx_new_window(gm->cnct, m->len, m->height, "so_long, farewell, auf wiedersehen goodbye!");
 	if (!gm->win)
 		er->win = 1;
-	gm->wallimg = mlx_xpm_file_to_image(data->connection, "/Users/nataliaschardosim/42_core/rank02/so_long/sprites/wall.xpm", &size, &size);
-	gm->playimg = mlx_xpm_file_to_image(data->connection, "/Users/nataliaschardosim/42_core/rank02/so_long/sprites/player.xpm", &size, &size);
-	gm->coinimg = mlx_xpm_file_to_image(data->connection, "/Users/nataliaschardosim/42_core/rank02/so_long/sprites/collectible.xpm", &size, &size);
-	gm->exitimg = mlx_xpm_file_to_image(data->connection, "/Users/nataliaschardosim/42_core/rank02/so_long/sprites/exit.xpm", &size, &size);
-	gm->tileimg = mlx_xpm_file_to_image(data->connection, "/Users/nataliaschardosim/42_core/rank02/so_long/sprites/tile.xpm", &size, &size);
+	gm->wallimg = mlx_xpm_file_to_image(data->connection, "/Users/nataliaschardosim/42_core/rank02/so_long/sprites/wall.xpm", &gm->size, &gm->size);
+	gm->playimg = mlx_xpm_file_to_image(data->connection, "/Users/nataliaschardosim/42_core/rank02/so_long/sprites/player.xpm", &gm->size, &gm->size);
+	gm->coinimg = mlx_xpm_file_to_image(data->connection, "/Users/nataliaschardosim/42_core/rank02/so_long/sprites/collectible.xpm", &gm->size, &gm->size);
+	gm->exitimg = mlx_xpm_file_to_image(data->connection, "/Users/nataliaschardosim/42_core/rank02/so_long/sprites/exit.xpm", &gm->size, &gm->size);
+	gm->tileimg = mlx_xpm_file_to_image(data->connection, "/Users/nataliaschardosim/42_core/rank02/so_long/sprites/tile.xpm", &gm->size, &gm->size);
 	if (!gm->wallimg || !gm->playimg || !gm->coinimg || !gm->exitimg || !gm->tileimg)
 		er->image = 1;
-	gm->size = 32;
+	gm->pl_x = m.pl_x;
+	gm->pl_y = m.pl_y;
+	gm->count_C = m.count_C;
 }
