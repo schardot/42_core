5b6d5e843f2c924c0b8d8c3f41525a5902ce67bf
Natália Schardosim <natalia.schardosim@hotmail.com>
2024-10-01 11:34:55 +0200

    working on checking valid move

--- rank02/so_long/include/so_long.h
+++ rank02/so_long/include/so_long.h
@@ -1,22 +1,43 @@
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
+#include "../include/minilibx_opengl_20191021/mlx.h"
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
+#include "../include/libft/libft.h"
+#include "../include/libft/get_next_line/get_next_line.h"
+#include "../include/map.h"
+#include <string.h>
+
+#define ESC 65307
+#define LEFT 65361
+#define UP 65362
+#define RIGHT 65363
+#define DOWN 65364
 
 typedef struct v_list
 {
-	void    *connection;
-	void    *window;
-	void    *img;
+	void    *cnct;
+	void    *win;
 	void    *wallimage;
 	void    *playerimage;
 	void    *coinimage;
 	void    *exitimage;
 	void    *tileimage;
+	int		size;
+	char	**map;
+	int		pl_x;
+	int		pl_y;
 } t_game;
+
+typedef struct a_list
+{
+	int	cnct; //no connection
+	int	win; //no window
+	int image;
+} t_gerr;
 
 int ft_update(t_game *g);
 t_game *init_data(t_game *data);
--- rank02/so_long/srcs/so_long.c
+++ rank02/so_long/srcs/so_long.c
@@ -1,169 +1,102 @@
-#include "../include/minilibx-linux/mlx.h"
 #include "../include/so_long.h"
-#include "../include/libft/libft.h"
-#include "../include/libft/get_next_line/get_next_line.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <errno.h>
-#include <string.h>
-
-int ft_update(t_game *g);
+#include "../include/map.h"
 
 int main(int argc, char **argv)
 {
-    t_game *data;
-    char **grid;
+	t_maperr	*merror;
+	t_map		*mstruct;
+	t_game		*gm;
+	char		**map;
 
-    //antes de inicializar struct, arg check
-    grid = start_map(argv[1]);
-    data = init_data(data);
+	init_map_structs(mstruct, merror);
+	gm->map = check_map(merror, mstruct, argv[1]);
+	// Initialize game structure, window, images, etc.
+	init_game_struct(gm, merror, mstruct);
+
+	// Set up the loop hook
+	mlx_loop_hook(gm.cnct, main_loop, &gm);
+
+	// Register the key press event handler
+	mlx_key_hook(gm.win, key_press, &gm);
+
+	// Start the main event loop
+	mlx_loop(gm.cnct);
+	newgame(map);
 }
-
-int ft_update(t_game *g)
+int	key_press(int key, t_game *gm)
 {
-    mlx_put_image_to_window(g->connection, g->window, g->img, 100, 100);
-    return (0);
+	static int	coin = 0;
+	if(check_valid_move())
+	{
+		gm->map[pl_y][pl_x] = '0';
+		if (key == RIGHT)
+			gm->map[pl_y][pl_x + 1] = 'P';
+		else if (key == LEFT)
+			gm->map[pl_y][pl_x - 1] = 'P';
+		else if (key == UP)
+			gm->map[pl_y + 1][pl_x] = 'P';
+		else if (key == DOWN)
+			gm->map[pl_y - 1][pl_x] = 'P';
+		else if (key == ESC)
+			fechar janela;
+	}
 }
-t_game *init_data(t_game *data)
+
+int main_loop(t_game *gm)
 {
-    data = (t_game *)malloc(1 * sizeof(t_game));
-    data->connection = mlx_init();
-    if (!data->connection)
-    {
-        printf("In init_data.c: Can't establish connection");
-        exit(1);
-    }
-    data->window = mlx_new_window(data->connection, 800, 500, "so_long, farewell, auf wiedersehen goodbye!"); // tem q vincular esse tamanho com o tamanho do mapa
-    if (!data->window)
-    {
-        printf("In init_data.c: Can't open window");
-        exit(1);
-    }
-    put_file_to_image(data);
-    mlx_loop_hook(data->connection, ft_update, (void *)data);
-    mlx_loop(data->connection);
-    return (data);
+	render_game(gm->map, gm);
+	return (0);
 }
-void put_file_to_image(t_game *data)
+
+void	render_game(char **map, t_game *gm)
 {
-    //tem q initializar data?
-    int size = 32;
-    data->coinimage = mlx_xpm_file_to_image(data->connection, "/Users/nataliaschardosim/42_core/rank02/so_long/sprites/collectible.xpm", &size, &size);
-    data->exitimage = mlx_xpm_file_to_image(data->connection, "/Users/nataliaschardosim/42_core/rank02/so_long/sprites/exit.xpm", &size, &size);
-    data->wallimage = mlx_xpm_file_to_image(data->connection, "/Users/nataliaschardosim/42_core/rank02/so_long/sprites/wall.xpm", &size, &size);
-    data->playerimage = mlx_xpm_file_to_image(data->connection, "/Users/nataliaschardosim/42_core/rank02/so_long/sprites/exit.xpm", &size, &size);
-    data->tileimage = mlx_xpm_file_to_image(data->connection, "/Users/nataliaschardosim/42_core/rank02/so_long/sprites/tile.xpm", &size, &size);
+	void	*img;
+	int		h;
+	int		w;
+
+	h = -1;
+	while (map[++h])
+	{
+		w = -1;
+		while (map[h][++w])
+		{
+			if (map[h][w] == '1')
+				img = gm->wallimage;
+			else if (map[h][w] == '0')
+				img = gm->tileimage;
+			else if (map[h][w] == 'C')
+				img = gm->coinimage;
+			else if (map[h][w] == 'E')
+				img = gm->exitimage;
+			else
+			{
+				img = gm->playerimage;
+				gm->pl_x = w;
+				gm->pl_y = h;
+			}
+			mlx_put_image_to_window(gm->cnct, gm->win, img, w * gm->size, h * gm->size);
+		}
+	}
 }
-char **start_map(char *filename)
+
+
+void init_game_struct(t_game *gm, t_gerr er, t_map m)
 {
-    check_extension(filename);
-    check_file(filename);
+	int	size;
+
+	size = 32;
+	gm->cnct = mlx_init();
+	if (!gm->cnct)
+		er->cnct = 1;
+	gm->win = mlx_new_window(gm->cnct, m->len, m->height, "so_long, farewell, auf wiedersehen goodbye!");
+	if (!gm->win)
+		er->win = 1;
+	gm->wallimg = mlx_xpm_file_to_image(data->connection, "/Users/nataliaschardosim/42_core/rank02/so_long/sprites/wall.xpm", &size, &size);
+	gm->playimg = mlx_xpm_file_to_image(data->connection, "/Users/nataliaschardosim/42_core/rank02/so_long/sprites/player.xpm", &size, &size);
+	gm->coinimg = mlx_xpm_file_to_image(data->connection, "/Users/nataliaschardosim/42_core/rank02/so_long/sprites/collectible.xpm", &size, &size);
+	gm->exitimg = mlx_xpm_file_to_image(data->connection, "/Users/nataliaschardosim/42_core/rank02/so_long/sprites/exit.xpm", &size, &size);
+	gm->tileimg = mlx_xpm_file_to_image(data->connection, "/Users/nataliaschardosim/42_core/rank02/so_long/sprites/tile.xpm", &size, &size);
+	if (!gm->wallimg || !gm->playimg || !gm->coinimg || !gm->exitimg || !gm->tileimg)
+		er->image = 1;
+	gm->size = 32;
 }
-void    check_extension(filename)
-{
-    char    *extension;
-
-    extension = ft_substr(filename, ft_strlen(filename) - 4, 4);
-    if (ft_strncmp(extension, ".ber", 5) != 0)
-    {
-        ft_printf("Error, the map is not .ber");
-        free (extension);
-        exit (1);
-    }
-    free (extension);
-}
-
-int check_file(char *filename)
-{
-    int fd;
-    char *line;
-    int linelen;
-    int auxlen;
-
-    fd = open(filename, O_RDONLY);
-    if (fd < 0)
-    {
-        ft_printf("In start_map, line 78, couldnt open file");
-        exit (1);
-    }
-    linelen = 0;
-    line = "a";
-    while (line)
-    {
-        line = get_next_line(fd);
-        check_chars(line);
-        auxlen = ft_strlen(line);
-        if (linelen > 0 && auxlen != linelen)
-        {
-            ft_printf("All the lines must have the same length");
-            free (line);
-            exit (1);
-        }
-        linelen = auxlen;
-        free (line);
-    }
-    close(fd);
-    return (linelen);
-}
-    // -------------------
-
-    // -------------------- III) funcao que vai ler uma linha e checar as unicas letras permitidas
-void    check_chars(char *line, int linelen)
-{
-    int i;
-    map_aux *t_map;
-
-    init_m_list(t_map);
-    i = 0;
-    if (line[0] != '1' || line[linelen - 1] != 1)
-    {
-        ft_printf("Borders are not all 1").
-        free (line);
-        exit (1);
-    }
-    while(line[i])
-    {
-        if (!ft_strchr("10PCE", line[i]))
-        {
-            ft_printf("Found and invalid character");
-            free (line);
-            exit (1);
-        }
-        i ++;
-    }
-}
-    // ---------------------
-
-    // --------------------- IV) criar o mapa
-char    **create_map()
-{
-    char **map;
-    int m;
-    int fd;
-
-    m = 0;
-    map = (char **)malloc(height * sizeof(char *));
-    if (!map)
-    {
-        ft_printf("Couldn't allocate map");
-        exit (1);
-    }
-    fd = open(filename, O_RDONLY);
-    if (fd < 0)
-    {
-        ft_printf("In creating the map, second read. Couldnt open file");
-        exit(1);
-    }
-    while (map[m])
-    {
-        map[m] = get_next_line(fd);
-        m ++;
-    }
-    return (map);
-}
-
-    // --------------------
-
-    // -------------------- V) ver se tem path valido e ver se a primeira e ultima linha sao '1'
-    nao quero fazer isso agora
-    // -------------------
